f0ffe7c61931dbb2ecff5889891c48e1b8b02caa
Camilo Ocampo <camiloocampo@Camilos-MacBook-Pro.local>
2020-08-07 07:23:59 +0200

    Implement copy constructor

--- Board.java
+++ Board.java
@@ -1,12 +1,30 @@
 public class Board {
 
+    private int[][] board;
+
+    public final void swapTiles(int x1, int y1, int x2, int y2){
+        int temp = this.board[x1][y1];
+        this.board[x1][y1] = this.board[x2][y2];
+        this.board[x2][y2]= temp;
+
+    }
+    //copy constructor
+    private Board(Board b){
+        this.board = new int[b.dimension()][b.dimension()];
+        for(int i = 0; i< b.dimension(); i ++) {
+            for (int j = 0; j < b.dimension(); j++) {
+                this.board[i][j] = b.board[i][j];
+            }
+        }
+    }
     private static int[][] goalArray = {
             { 1, 2, 3 },
             { 4, 5, 6 },
             { 7, 8, 0 }
     };
+
     private static Board goalBoard = new Board(goalArray);
-    private int[][] board;
+
 
 
     // create a board from an n-by-n array of tiles,
@@ -107,6 +125,8 @@ public class Board {
 
         return this.equals(goalBoard);
     }
+
+
   /*//
 
 
@@ -135,6 +155,11 @@ public class Board {
         System.out.println( b.manhattan());
         System.out.println(b.equals(c));
         System.out.println(b.isGoal());
+        Board test = new Board(b);
+
+        test.swapTiles(0, 0, 0, 1);
+        System.out.println(test.toString());
+        System.out.println(b.toString());
     }
 
 }
